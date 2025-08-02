b7db1f119a1e8e1580adb33bd8b7825da10959d2
Zeyan <zeyan.du@rea-group.com>
2025-08-02 16:25:33 +0800

    Revert "Add more maven repo"
    This reverts commit 1b2fd6fa47b521efa805da5fc241ab8b08e1134b.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,74 +3,79 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 val ktorVersion = "2.3.12"
 
 plugins {
-    kotlin("jvm") version "2.1.20"
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20"
-    id("org.jetbrains.compose") version "1.6.11"
-    id("org.jlleitschuh.gradle.ktlint") version "12.3.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+  kotlin("jvm") version "2.1.20"
+  id("org.jetbrains.kotlin.plugin.compose") version "2.1.20"
+  id("org.jetbrains.compose") version "1.6.11"
+  id("org.jlleitschuh.gradle.ktlint") version "12.3.0"
+  id("com.github.johnrengelman.shadow") version "8.1.1"
+  id("edu.sc.seis.launch4j") version "2.5.0"
 }
 
 group = "io.github.gonborn"
 version = "1.0.0"
 
 repositories {
-    mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    maven("https://maven.aliyun.com/repository/public")
-    maven("https://maven.aliyun.com/repository/jcenter")
-    maven("https://maven.aliyun.com/repository/google")
-    google()
+  mavenCentral()
+  maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+  google()
 }
 
 dependencies {
-    // Ktor
-    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-html-builder-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-websockets-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-client-websockets:$ktorVersion")
-    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+  // Ktor
+  implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-html-builder-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-websockets-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-client-websockets:$ktorVersion")
+  implementation("io.ktor:ktor-client-cio:$ktorVersion")
 
-    // Other
-    implementation("org.slf4j:slf4j-simple:2.0.13")
-    implementation("net.lingala.zip4j:zip4j:2.11.5")
-    implementation("com.google.zxing:core:3.5.3")
-    implementation("com.google.zxing:javase:3.5.3")
+  // Other
+  implementation("org.slf4j:slf4j-simple:2.0.13")
+  implementation("net.lingala.zip4j:zip4j:2.11.5")
+  implementation("com.google.zxing:core:3.5.3")
+  implementation("com.google.zxing:javase:3.5.3")
 
-    // Compose
-    implementation(compose.desktop.currentOs)
+  // Compose
+  implementation(compose.desktop.currentOs)
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 tasks.build {
-    dependsOn(tasks.shadowJar)
+  dependsOn(tasks.shadowJar)
 }
 
 kotlin {
-    jvmToolchain(21)
+  jvmToolchain(21)
 }
 
 compose.desktop {
-    application {
-        mainClass = "MainKt"
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "SuperShare"
-            packageVersion = "1.0.0"
-        }
+  application {
+    mainClass = "MainKt"
+    nativeDistributions {
+      targetFormats(TargetFormat.Dmg, TargetFormat.Deb) // Keep DMG for macOS, remove MSI for Windows
+      packageName = "SuperShare"
+      packageVersion = "1.0.0"
     }
+  }
+}
+
+launch4j {
+    mainClassName = "MainKt"
+    jar = tasks.shadowJar.get().archiveFile.get().asFile.absolutePath
+    outfile = "dist/SuperShare.exe"
+    headerType = "gui"
 }
 
 tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
-    archiveBaseName.set("super-share")
-    archiveClassifier.set("")
+  archiveBaseName.set("super-share")
+  archiveClassifier.set("")
 
-    manifest {
-        attributes(
-            "Main-Class" to "MainKt",
-        )
-    }
+  manifest {
+    attributes(
+      "Main-Class" to "MainKt",
+    )
+  }
 }
